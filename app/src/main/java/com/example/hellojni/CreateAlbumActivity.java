b375b34a84236f5bf9d2b23cd897c0b6d0e4d267
john <sato.keita@furyu.jp>
2016-04-24 02:53:59 +0900

    albumのレイアウト作成中

--- app/src/main/java/com/example/hellojni/CreateAlbumActivity.java
+++ app/src/main/java/com/example/hellojni/CreateAlbumActivity.java
@@ -1,14 +1,24 @@
 package com.example.hellojni;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.GridView;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-import com.example.hellojni.adapter.CreateAlbumListAdapter;
+import com.example.hellojni.adapter.MuteVideoAdapter;
 import com.example.hellojni.model.Album;
 import com.example.hellojni.model.Image;
 import com.example.hellojni.model.Video;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +28,9 @@ import java.util.List;
 public class CreateAlbumActivity extends Activity {
 
     private String moviewPath = "/Movies/preload_xperia_hd2.mp4";
-    private String moviewPath2 = "/Movies/preload_ps4_montage.mp4";
-    List<Album> albums = new ArrayList<>();
+    private String imagePath = "/image/alpha_amalfi_coast.jpg";
+
+    LinearLayout linearLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,9 +38,60 @@ public class CreateAlbumActivity extends Activity {
         setContentView(R.layout.create_album_activity);
 
         // TODO:Videoを組み立てる手段
-        albums.add(new Album(new Video(moviewPath2), new Image("")));
-        GridView gridView = (GridView) findViewById(R.id.create_album_activity_list);
-        gridView.setAdapter(new CreateAlbumListAdapter(this, 0, albums));
+        List<Album> albums = new ArrayList<>();
+        {
+            albums.add(new Album(new Video(moviewPath), new Image("")));
+            albums.add(new Album(new Video(""), new Image(imagePath)));
+            albums.add(new Album(new Video(""), new Image("")));
+        }
+
+        // folderがあるかないかを見る
+        String.valueOf(R.string.directory_name);
+
+        linearLayout = (LinearLayout) findViewById(R.id.content_create_album_layout_01);
+        View resource = createResource(albums.get(0));
+        if (resource != null) {
+            linearLayout.addView(resource);
+        } else {
+            linearLayout.setBackgroundResource(R.drawable.select_album_design_01_bgi);
+        }
+        linearLayout = (LinearLayout) findViewById(R.id.content_create_album_layout_02);
+        resource = createResource(albums.get(1));
+        if (resource != null) {
+            linearLayout.addView(resource);
+        } else {
+            linearLayout.setBackgroundResource(R.drawable.select_album_design_01_bgi);
+        }
+        linearLayout = (LinearLayout) findViewById(R.id.content_create_album_layout_03);
+        resource = createResource(albums.get(2));
+        if (resource != null) {
+            linearLayout.addView(resource);
+        } else {
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(R.drawable.select_album_design_01_bgi);
+            linearLayout.addView(imageView);
+        }
+    }
+
+    private View createResource(Album album) {
+        if (album.hasImage()) {
+            ImageView imageView = new ImageView(this);
+            File file = new File(Environment.getExternalStorageDirectory().getPath() + album.image.path);
+            if (file.exists()) {
+                imageView.setImageBitmap(BitmapFactory.decodeFile(file.getPath()));
+            }
+            return imageView;
+        } else if (album.hasVideo()) {
+            VideoView videoView = new VideoView(this);
+            MediaController mc = new MediaController(this);
+            mc.setAnchorView(videoView);
+            mc.setMediaPlayer(videoView);
+            videoView.setOnPreparedListener(new MuteVideoAdapter());
+            videoView.setVideoPath(Environment.getExternalStorageDirectory().getPath() + album.video.path);
+            videoView.start();
+            return videoView;
+        }
+        return null;
     }
 
     @Override
